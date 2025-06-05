cfb3c5960cf7e3330cc16bc13ce676c9ff07bae0
anikmk <anikmollik122321@gmail.com>
2025-06-06 00:09:00 +0600

    make calculate for admin

--- client/src/Pages/Dashboard/Calculator/Calculator.jsx
+++ client/src/Pages/Dashboard/Calculator/Calculator.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+
 const Calculator = () => {
+    const [money,setMoney] = useState(0);
+    const handleSaveMoney = (e)=>{
+        const price = e.target.value;
+       setMoney(price)
+       price.reset()
+    }
+     const handleClick = () => {
+        console.log(money);
+
+        }
     return <>
-    <div>calculators</div>
+    <div>
+        <div><h1>calculators</h1></div>
+        <div>{money}</div>
+        <div className="flex items-center gap-3">
+            <div><label htmlFor="">input number</label><input onChange={handleSaveMoney} name="money" type="number" />
+            </div>
+            <div><button onClick={handleClick}>save</button></div>
+        </div>
+    </div>
     </>
 }
 export default Calculator;
